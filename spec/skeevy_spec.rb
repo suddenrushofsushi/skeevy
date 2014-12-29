4bfed00fb98febd423a5dbe28870426e1df18169
Craig Waterman <craigwaterman@gmail.com>
2014-12-29 14:41:10 -0500

    Adds guard-rspec dev dep. Adds Guardfile with rails/capybara examples removed. Adds initial spec.

---
+++ spec/skeevy_spec.rb
@@ -0,0 +1,23 @@
+require 'skeevy'
+
+RSpec.describe :skeevy do
+
+  describe :register! do
+    it 'requires an identifier' do
+      expect { Skeevy.register! }.to raise_error(ArgumentError, /identifier/)
+    end
+
+    it 'validates engine type' do
+      expect {
+        Skeevy.register!(identifier: :test, engine: "My Engine")
+      }.to raise_error(ArgumentError, /Engine!/)
+    end
+
+    it 'validates cutter type' do
+      expect {
+        Skeevy.register!(identifier: :test, cutter: "My Cutter")
+      }.to raise_error(ArgumentError, /Cutter!/)
+    end
+  end
+
+end

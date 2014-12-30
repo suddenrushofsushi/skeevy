ef72e07048ad8d8fade439aeb0848923e35aa7d1
Craig Waterman <craigwaterman@gmail.com>
2014-12-30 11:40:38 -0500

    Adds impl specific tests for Engines:DF

--- spec/lib/skeevy/engines/directory_file_spec.rb
+++ spec/lib/skeevy/engines/directory_file_spec.rb
@@ -1,10 +1,33 @@
+require 'fileutils'
+
 RSpec.describe :directory_file do
 
   let!(:dfs) { Skeevy::Engines::DirectoryFile.new(instance: nil, base_dir: 'tmp', delimiter: '-') }
+  let!(:write_key) { 'a-b-c-file.json' }
   include_examples "a skeevy engine"
 
-  describe 'E:DF specific tests' do
-    pending
+  describe '.write! verification' do
+    it 'should create the appropriate directory' do
+      write_file
+      expect(File.directory?('tmp/a/b/c')).to be_truthy
+    end
+
+    it 'should create the appropriate file' do
+      write_file
+      expect(File.exist?('tmp/a/b/c/file.json')).to be_truthy
+    end
+  end
+
+  describe '.delete! verification' do
+    it 'should remove the file' do
+      write_file
+      dfs.delete!(key: write_key)
+      expect(File.exist?('tmp/a/b/c/file.json')).to be_falsey
+    end
+  end
+
+  def write_file
+    dfs.write!(key: write_key, content: '{}')
   end
 
 end
